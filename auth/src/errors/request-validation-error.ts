0ee74d8e74ac88459298cde8e1719eb335f73adc
Rahul S Kumar <rahulskumar@Rahuls-MacBook-Air.local>
2024-08-02 12:29:03 +0530

    error handler with abstract class fo custom error and async call changes

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,17 +1,19 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-interface CustomError {
-  statusCode: number;
-  serializeErrors(): {
-    message: string;
-    field?: string;
-  }[];
-}
+// interface CustomError {
+//   statusCode: number;
+//   serializeErrors(): {
+//     message: string;
+//     field?: string;
+//   }[];
+// }
 
-export class RequestValidationError extends Error implements CustomError {
+// export class RequestValidationError extends Error implements CustomError {
+export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid request parameters');
 
     //Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
+// import { RequestValidationError } from '../errors/request-validation-error';
+// import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -8,19 +9,23 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    //console.log('handling this error as Request validation error');
-    // const formattedErrors = err.errors.map((error) => {
-    //   if (error.type === 'field') {
-    //     return { message: error.msg, field: error.path };
-    //   }
-    // });
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
+  // if (err instanceof RequestValidationError) {
+  //   //console.log('handling this error as Request validation error');
+  //   // const formattedErrors = err.errors.map((error) => {
+  //   //   if (error.type === 'field') {
+  //   //     return { message: error.msg, field: error.path };
+  //   //   }
+  //   // });
+  //   return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  // }
 
-  if (err instanceof DatabaseConnectionError) {
-    //console.log('handling this error as Database connection error');;
-    //return res.status(500).send({ errors: [{ message: err.reason }] });
+  // if (err instanceof DatabaseConnectionError) {
+  //   //console.log('handling this error as Database connection error');;
+  //   //return res.status(500).send({ errors: [{ message: err.reason }] });
+  //   return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  // }
+
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
