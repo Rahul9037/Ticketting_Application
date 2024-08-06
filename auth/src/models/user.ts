0d5b944812e8fcfaec5e00d16012b94c36684236
Rahul S Kumar <rahulskumar@Rahuls-MacBook-Air.local>
2024-08-06 10:02:53 +0530

    user sign up initial implementation

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,8 +1,21 @@
-import mongoose, { trusted } from 'mongoose';
-
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
 //interface that describe the properties that are required to create a new user
 
 interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+//interface that describe the properties that a user model has
+
+//initailly -  interface UserModel extends mongoose.Model<any> {
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attr: UserAttrs): UserDoc;
+}
+
+//interface that descriobes the properties that a user document has
+interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -18,10 +31,28 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+//presave
 
-const buildUser = (attr: UserAttrs) => {
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+//add statc method to schema
+
+userSchema.statics.build = (attr: UserAttrs) => {
   return new User(attr);
 };
 
-export { User, buildUser };
+// initially - const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+// const buildUser = (attr: UserAttrs) => {
+//   return new User(attr);
+// };
+
+// export { User, buildUser };
+export { User };
