71858c64871fb413c9739ca2bf1e542aa6fb2788
Rahul S Kumar <rahulskumar@Rahuls-MacBook-Air.local>
2024-08-07 10:08:17 +0530

    sign out and current user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!!!!!');
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+  //since we are using the below code insde the currentuser middleware
+  // if (!req.session?.jwt) {
+  //   res.send({ currentUser: null });
+  // }
+  // try {
+  //   const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+  //   res.send({ currentUser: payload });
+  // } catch (error) {
+  //   res.send({ currentUser: null });
+  // }
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as curentUserRouter };
